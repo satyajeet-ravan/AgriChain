5867c45b9e65c4a54e50cc626ba1e57b094b172b
Rajgaurav Patil <rajgauravpati06@gmail.com>
2026-04-07 01:33:33 +0530

    Refactor navigation and styling, add phone input, and update theme colors

--- frontend/src/pages/sign-up.jsx
+++ frontend/src/pages/sign-up.jsx
@@ -2,9 +2,9 @@ import PhoneInput from "react-phone-number-input"
 import "./sign-up.css"
 import { useState } from "react"
 import { FaEye, FaEyeSlash } from "react-icons/fa"
-import Home from "./home"
 import { useNavigate } from "react-router-dom"
-
+import Farmer from "../assets/Farmer.jpg"
+import Buyer from "../assets/buyer.jpg"
 
 function Signup() {
     const [phone, setPhone] = useState("")
@@ -12,6 +12,7 @@ function Signup() {
     const [confirmPassword, setComfirmPassword] = useState("")
     const [showPassword, setShowPassword] = useState(false)
     const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+    const [selected, setSelected] = useState("")
     const navigation = useNavigate()
     return (
         <>
@@ -19,8 +20,13 @@ function Signup() {
                 <form action="" className="sign-up-card">
                     <h2>Create Your Account</h2>
                     <input type="text" name="Name" placeholder="Enter Your Name" required />
-                    <input type="email" name="Email" placeholder="Enter Your Email" required />
+                    <input type="email" name="Email" placeholder="Enter Your Email" />
                     <PhoneInput defaultCountry="IN" value={phone} onChange={setPhone} placeholder="Enter Your Phone Number" />
+                    <h4>Select Login Type: </h4>
+                    <div className="login-type">
+                    <button type="select" onClick={() => setSelected("farmer")} disabled={selected == "buyer"}><img src={Farmer} alt="Farmer-signin-button" /> Farmer/Seller </button>
+                    <button type="select" onClick={() => setSelected("buyer")} disabled={selected == "farmer"}><img src={Buyer} alt="buyer-signin-button" /> Customer/Buyer </button>
+                    </div>
                     <div className="password-wrapper">
                         <input type={showPassword ? "text" : "password"}
                             name="Password"
